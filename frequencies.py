1e753e821ab565c2c04f640bf222507a0abd3ecf
Abirami Muthukumar <abirami.muthukumar@kcl.ac.uk>
2023-10-14 10:57:22 +0100

    Complete frequencies implementation

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = [str(item) for item in items]
+
+    while len(items) > 0:
+        # get first element, count how many times it's in the items list using count(), then remove it from items
+        target_element = items[0]
+        frequencies[target_element] = items.count(target_element)
+        items = [item for item in items if item!=target_element]
+
     return frequencies
